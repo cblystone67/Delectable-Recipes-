a82c1815b47bad534afea101c475bfb2a018ac1b
cblystone67 <christopherblystone1@gmail.com>
2023-07-13 10:50:24 -0700

    Nav update

--- src/components/Category.jsx
+++ src/components/Category.jsx
@@ -7,19 +7,31 @@ import React from 'react'
 
 function Category() {
   return (
-    <div>
+    <List>
       <div>
         <FaPizzaSlice />
         <h4>Italian</h4>
+      </div>
+      <div>
         <FaHamburger />
         <h4>American</h4>
+      </div>
+      <div>
         <GiNoodles />
         <h4>Thai</h4>
+      </div>
+      <div>
         <GiChopsticks />
         <h4>Japanese</h4>
       </div>
-    </div>
-  )
+    </List>
+  );
 }
+
+const List = styled.div`
+  display: flex;
+  justify-content: center;
+  margin: 2rem 0rem;
+  `;
 
 export default Category
