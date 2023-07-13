c005718497141a4c414c284477cd76177e2b271a
cblystone67 <christopherblystone1@gmail.com>
2023-07-13 09:41:42 -0700

    Edited Image and Text on Pop page

--- src/components/Popular.jsx
+++ src/components/Popular.jsx
@@ -35,6 +35,7 @@ const [popular, setPopular] = useState([]);
                   <Card>
                     <p>{recipe.title}</p>
                     <img src={recipe.image} alt={recipe.title}></img>
+                    <Gradient />
                   </Card>
                 </SplideSlide>
               );
@@ -51,11 +52,38 @@ const Card = styled.div`
   min-height: 25rem;
   border-radius: 2rem;
   overflow: hidden;
+  position: relative;
   
   img{
     border-radius: 2rem;
+    position: absolute;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+  }
+  p{
+    position: absolute;
+    z-index: 10;
+    left: 50%;
+    bottom: 0%;
+    transform: translate(-50%, 0%);
+    color: white;
+    width: 100%;
+    text-align: center;
+    font-weigt: 600;
+    font-size: 1rem;
+    height: 40%;
+    display: flex;
+    justify-content: center;
   }
   `;
-
+const Gradient = styled.div`
+  z-index: 3;
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  background: linear-gradient(rgba(0,0,0,0), rgba(0,0,0,0.5));
+  `
 
 export default Popular
