6dbe43a042f44d8b37bc7634244bc411829f3847
cblystone67 <christopherblystone1@gmail.com>
2023-07-13 21:34:14 -0700

    useLocation and motion for animation

--- src/pages/Cuisine.jsx
+++ src/pages/Cuisine.jsx
@@ -18,7 +18,11 @@ function Cuisine() {
     console.log(params.type)
   }, [params.type]);
   return (
-    <Grid>
+    <Grid
+      animate={{opacity: 1}}
+      initial={{opacity: 0}}
+      exit={{opacity: 0}}
+      transition={{duration: 0.5}}>
       {cuisine.map((item) => {
         return(
           <Card key={item.id}>
